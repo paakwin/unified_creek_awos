a8e45acc025d04b5a385f1e750740c0347151227
paakwin <paakwin0@gmail.com>
2025-07-07 08:49:27 +0500

    autorun

--- start_awos.sh
+++ start_awos.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# Wait for desktop environment to fully load (important for GUI apps)
+sleep 10
+
+# Change to project directory 
 cd /home/asif/Desktop/unified_creek_awos
+
+# Activate virtual environment
 source .venv/bin/activate
-export DISPLAY=:0
+
+# Run the Python script
 python awos.py
